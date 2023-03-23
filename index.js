476f34670cb3ca31047a20a6006edd3b477b8d15
RoosLearn <105787041+RoosLearn@users.noreply.github.com>
2023-03-23 10:25:48 +0530

    Updated Index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ let transporter = nodemailer.createTransport({
   });
 
   let mailOptions = {
-    from: 'panchoosparadise@gmail.com',
-    to: 'jwbanija@gmail.com',
+    from: 'xxx@gmail.com',
+    to: 'yyy@gmail.com',
     subject: 'Nodemailer Project',
     html: '<h1>Hello There, Welcome to your Email</h1><h2>This email is send to you using nodemailer.</h2><p>Here is an image for your Warm Welcome. Enjoy !!! </p><a href="https://www.animatedimages.org/cat-good-morning-1225.htm"><img src="https://www.animatedimages.org/data/media/1225/animated-good-morning-image-0025.gif" border="0" alt="animated-good-morning-image-0025" /></a><h3>Write back if you need to know more</h3><a href="https://www.animatedimages.org/cat-welcome-707.htm"><img src="https://www.animatedimages.org/data/media/707/animated-welcome-image-0126.gif" border="0" alt="animated-welcome-image-0126" /></a>'
   };
